8676f2ad9756822b1f0482971919c5652cb84ab2
Abdullah Alabasi <Tech3790@gmail.com>
2019-04-18 13:28:15 -0500

    removed unused code

--- src/views/Components/Sections/MenuAppBar.jsx
+++ src/views/Components/Sections/MenuAppBar.jsx
@@ -4,13 +4,6 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Switch from "@material-ui/core/Switch";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import FormGroup from "@material-ui/core/FormGroup";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
-
-library.add(faStroopwafel);
 
 const styles = {
   root: {
@@ -34,80 +27,53 @@ class MenuAppBar extends React.Component {
     anchorEl: null
   };
 
-  handleChange = event => {
-    this.setState({ auth: event.target.checked });
-  };
-
   handleMenu = event => {
     this.setState({ anchorEl: event.currentTarget });
   };
 
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
   render() {
-    const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
+    const { anchorEl } = this.state;
     const open = Boolean(anchorEl);
 
     return (
-      <div className={classes.root}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={auth}
-                onChange={this.handleChange}
-                aria-label="LoginSwitch"
-              />
-            }
-            label={auth ? "Logout" : "Login"}
-          />
-        </FormGroup>
-        <AppBar
-          position="static"
-          color="secondary"
-          style={{ padding: "0px", height: "58px" }}
-        >
-          <Toolbar>
-            {auth && (
-              <div>
-                <IconButton
-                  aria-owns={open ? "menu-appbar" : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                  className="appBarButton"
-                >
-                  <i className="fas fa-columns" style={{ color: "white" }} />
-                  <p style={{ color: "white", margin: "10px" }}>Dashboard</p>
-                </IconButton>
-                <IconButton
-                  aria-owns={open ? "menu-appbar" : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                  className="appBarButton"
-                >
-                  <i className="fas fa-trophy" style={{ color: "white" }} />
-                  <p style={{ color: "white", margin: "10px" }}>Trophies</p>
-                </IconButton>
-                <IconButton
-                  aria-owns={open ? "menu-appbar" : undefined}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                  className="appBarButton"
-                >
-                  <i className="fas fa-comments" style={{ color: "white" }} />
-                  <p style={{ color: "white", margin: "10px" }}>Messages</p>
-                </IconButton>
-              </div>
-            )}
-          </Toolbar>
-        </AppBar>
-      </div>
+      <AppBar
+        position="static"
+        color="secondary"
+        style={{ padding: "0px", height: "58px", marginTop: "50px" }}
+      >
+        <Toolbar>
+          <IconButton
+            aria-owns={open ? "menu-appbar" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            color="inherit"
+            className="appBarButton"
+          >
+            <i className="fas fa-columns" style={{ color: "white" }} />
+            <p style={{ color: "white", margin: "10px" }}>Dashboard</p>
+          </IconButton>
+          <IconButton
+            aria-owns={open ? "menu-appbar" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            color="inherit"
+            className="appBarButton"
+          >
+            <i className="fas fa-trophy" style={{ color: "white" }} />
+            <p style={{ color: "white", margin: "10px" }}>Trophies</p>
+          </IconButton>
+          <IconButton
+            aria-owns={open ? "menu-appbar" : undefined}
+            aria-haspopup="true"
+            onClick={this.handleMenu}
+            color="inherit"
+            className="appBarButton"
+          >
+            <i className="fas fa-comments" style={{ color: "white" }} />
+            <p style={{ color: "white", margin: "10px" }}>Messages</p>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
